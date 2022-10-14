9ab8ef1b86e4b266b7f3efff19d25444d8bf9db3
Grabyy <gabriel.tourillon@gmail.com>
2022-10-14 23:50:53 +0200

    Mes deux supers test

---
+++ test_1.py
@@ -0,0 +1,6 @@
+def plut_un(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(4444) == 5
---
+++ test_2.py
@@ -0,0 +1,4 @@
+def test_list_list():
+   s1 = set(["Pomme", "Bannane", "Haricot"])
+   s2 = set(["Pomme", "Bannane", "Poire"])
+   assert s1 == s2
